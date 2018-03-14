826ba897d13e67fb6412657ccc7a8324e6e59d53
Tony Lin <tonylin1208@gmail.com>
2018-03-14 16:26:05 +0800

    add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
   root "artists#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #後台admin routes
+  namespace :admin do
+    resources :events, only: [:index, :edit, :update]
+    resources :users, only: [:index, :destroy]
+    root "events#index"
+  end
 end
