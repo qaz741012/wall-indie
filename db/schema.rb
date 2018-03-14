eae0c7e947491f4c1a1c35f9f30b141c9a9d4e5f
Tony Lin <tonylin1208@gmail.com>
2018-03-14 16:30:41 +0800

    add events and place db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313062950) do
+ActiveRecord::Schema.define(version: 20180314075037) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "photo"
     t.text "intro"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "intro"
+    t.date "date"
+    t.string "time"
+    t.string "ticket_link"
+    t.string "organizer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "tel"
+    t.text "info"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
