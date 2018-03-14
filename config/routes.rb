9adf2c1aff9bb2d9e2747c5992f0aadb9d508a10
MarkWeng <bskin0330@gmail.com>
2018-03-14 22:22:08 +0800

    Routes users show, edit, update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :artists
+  resources :users, only: [:show, :edit, :update]
 
   root "artists#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
