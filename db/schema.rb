cc5a7061d7fbd9aedc0dc110c0dd7125a0eb6a95
Chia-Hung Lin <qaz741012@gmail.com>
2018-03-15 14:11:50 +0800

    add email confirmation to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314152235) do
+ActiveRecord::Schema.define(version: 20180315060247) do
 
   create_table "artist_followships", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -110,6 +110,10 @@ ActiveRecord::Schema.define(version: 20180314152235) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
