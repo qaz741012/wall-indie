c2e31d8ced313d9033805c29befce3efd4841d81
Chia-Hung Lin <qaz741012@gmail.com>
2018-03-15 14:16:14 +0800

    enable email confirmation in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,15 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+
+  # 添加smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
