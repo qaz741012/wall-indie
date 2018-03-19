df9d4051a7091b5bb348a4d50ef18d38528b12e1
Tony Lin <tonylin1208@gmail.com>
2018-03-19 18:21:53 +0800

    change the root to events#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :artists
 
-  root "artists#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events, only: [:index]
+  root "events#index"
 
   #後台admin routes
   namespace :admin do
