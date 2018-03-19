aba42598077ca7e7719709cf41b38cdeef15df39
Chia-Hung Lin <qaz741012@gmail.com>
2018-03-19 16:40:35 +0800

    event#index template

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :events
+
   root "artists#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
