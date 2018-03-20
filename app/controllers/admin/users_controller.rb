cfb04fbce90402ab447b18a724e7effe0263b170
Tony Lin <tonylin1208@gmail.com>
2018-03-20 18:35:34 +0800

    add admin user view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,15 @@
 class Admin::UsersController < ApplicationController
   def index
-    
+    @users = User.all
   end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to artists_url, notice: 'Artist was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
 end
