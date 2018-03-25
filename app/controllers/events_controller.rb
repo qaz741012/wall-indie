ea2c8c4eb662610a0999fd2b6f6136574c1a3ff9
MarkWeng <bskin0330@gmail.com>
2018-03-25 20:48:48 +0800

    add marker infowindow on google map

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,7 @@ class EventsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@places) do |place, marker|
       marker.lat place.latitude
       marker.lng place.longitude
+      marker.infowindow place.info 
   end
   end
 
