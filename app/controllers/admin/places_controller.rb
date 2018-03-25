14954ebc5f5fd974a588aff229d7bf8af7e845b3
MarkWeng <bskin0330@gmail.com>
2018-03-25 20:19:14 +0800

    add admin controller create

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -6,4 +6,22 @@ class Admin::PlacesController < ApplicationController
   def new
     @place = Place.new
   end
+
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      flash[:notice] = "place was successfully create"
+      redirect_to admin_places_path
+    else
+      flash[:alert] = "place was failed to create"
+      render :new
+    end
+
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :address, :tel, :info, :latitude, :longitude)
+  end
 end
