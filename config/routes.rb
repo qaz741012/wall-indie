3c4d78beca74ffc662d0c728e9835279947ce5a3
Chia-Hung Lin <qaz741012@gmail.com>
2018-03-28 23:22:41 +0800

    debug style of registration and forgot password page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+                                    registrations: 'users/registrations',
+                                    passwords: 'users/passwords'}
   resources :artists do
     member do
       post :follow
