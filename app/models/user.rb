ca36fd9810e4b65dd7bcfb8a22ed35cce42ea194
Chia-Hung Lin <qaz741012@gmail.com>
2018-03-29 22:17:20 +0800

    automatically build user-follow-artist when user login by spotify

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,7 +52,7 @@ class User < ApplicationRecord
   # 處理facebook授權的資料
   def self.from_facebook_omniauth(auth)
     # Case 1: Find existing user by facebook uid
-    user = User.find_by_fb_uid( auth.uid )
+    user = User.find_by_fb_uid(auth.uid)
     if user
       user.fb_token = auth.credentials.token
       user.skip_confirmation!
@@ -61,8 +61,9 @@ class User < ApplicationRecord
     end
 
     # Case 2: Find existing user by email
-    existing_user = User.find_by_email( auth.info.email )
+    existing_user = User.find_by_email(auth.info.email)
     if existing_user
+      existing_user.provider = auth.provider
       existing_user.fb_uid = auth.uid
       existing_user.fb_token = auth.credentials.token
       existing_user.skip_confirmation!
@@ -72,6 +73,7 @@ class User < ApplicationRecord
 
     # Case 3: Create new password
     user = User.new
+    user.provider = auth.provider
     user.fb_uid = auth.uid
     user.fb_token = auth.credentials.token
     user.email = auth.info.email
@@ -84,7 +86,7 @@ class User < ApplicationRecord
   # 處理spotify授權的資料
   def self.from_spotify_omniauth(auth)
     # Case 1: Find existing user by facebook uid
-    user = User.find_by_spotify_uid( auth.uid )
+    user = User.find_by_spotify_uid(auth.uid)
     if user
       user.spotify_token = auth.credentials.token
       user.skip_confirmation!
@@ -93,10 +95,12 @@ class User < ApplicationRecord
     end
 
     # Case 2: Find existing user by email
-    existing_user = User.find_by_email( auth.info.email )
+    existing_user = User.find_by_email(auth.info.email)
     if existing_user
+      existing_user.provider = auth.provider
       existing_user.spotify_uid = auth.uid
       existing_user.spotify_token = auth.credentials.token
+      existing_user.follow_artist_from_spotify(auth)
       existing_user.skip_confirmation!
       existing_user.save!
       return existing_user
@@ -104,6 +108,7 @@ class User < ApplicationRecord
 
     # Case 3: Create new password
     user = User.new
+    user.provider = auth.provider
     user.spotify_uid = auth.uid
     user.spotify_token = auth.credentials.token
     user.email = auth.info.email
@@ -111,6 +116,30 @@ class User < ApplicationRecord
     user.skip_confirmation!
     user.save!
     return user
+  end
+
+  def follow_artist_from_spotify(auth)
+    user = RSpotify::User.new(auth)
+    sp_artists = user.following(type: 'artist')  # 取得使用者在Spotify追蹤的artist
+    sp_artists.each do |sp_artist|
+      ## 建立artist
+
+      artist = Artist.find_by_name(sp_artist.name)
+      # Case 1: 如果artist存在，但沒圖片 => 存圖片
+      if artist && !artist.photo?
+        artist.photo = sp_artist.images[0]["url"]
+
+      # Case 2: 如果artist不存在 => 存入artist
+      elsif !artist
+        artist = Artist.new
+        artist.name = sp_artist.name
+        artist.photo = sp_artist.images[0]["url"]
+      end
+      artist.save
+
+      ## 建立user-follow-artist關聯
+      artist_followship = self.artist_followships.build(artist_id: artist.id)
+    end
   end
 
 end
