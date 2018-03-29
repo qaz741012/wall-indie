596b47a77db52fe074038fa2ab6e3f9b274dc047
Tony Lin <tonylin1208@gmail.com>
2018-03-29 09:49:28 +0800

    mail test in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+
   def show
+    
     @user = User.find(params[:id])
+    # ====== mail test ======
+    # mail to user when he enter the user#show. It worked!
+    
+    # UserMailer.welcome_email(@user).deliver_now
     @followed_events = @user.user_followed_events
     @followed_artists = @user.user_followed_artists
     @favorited_artists = @user.favorited_artists
