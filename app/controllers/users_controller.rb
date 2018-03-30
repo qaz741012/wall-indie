f6c51654467a4ea0f0f0dcc5f6b2dca5bb380ed0
Tony Lin <tonylin1208@gmail.com>
2018-03-30 14:59:35 +0800

    add security of visitor entry user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,12 @@ class UsersController < ApplicationController
   def show
     
     @user = User.find(params[:id])
+    if !current_user
+      redirect_to root_path
+    end
     # ====== mail test ======
     # mail to user when he enter the user#show. It worked!
-    UserMailer.welcome_email(@user).deliver_now
+    # UserMailer.welcome_email(@user).deliver_now
     @followed_events = @user.user_followed_events
     @followed_artists = @user.user_followed_artists
     @favorited_artists = @user.favorited_artists
