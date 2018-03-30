836cc140c7738b6e24305cdec90d525a68f7fdc2
Tony Lin <tonylin1208@gmail.com>
2018-03-30 11:51:03 +0800

    add  mail notice of enter user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # ====== mail test ======
     # mail to user when he enter the user#show. It worked!
-    
-    # UserMailer.welcome_email(@user).deliver_now
+    UserMailer.welcome_email(@user).deliver_now
     @followed_events = @user.user_followed_events
     @followed_artists = @user.user_followed_artists
     @favorited_artists = @user.favorited_artists
