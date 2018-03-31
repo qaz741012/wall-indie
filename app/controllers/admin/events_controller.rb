1c5d91b0755209673d7eed7accfbdb0136d136c4
Tony Lin <tonylin1208@gmail.com>
2018-03-31 17:04:05 +0800

    add method of edit and update in admin event_controller

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -3,4 +3,25 @@ class Admin::EventsController < Admin::BaseController
   def index
     @events = Event.all
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      flash[:notice] = 'event was successfully update'
+      redirect_to admin_events_path
+    else
+      flash[:alert] = 'event was failed to create'
+      render :edit
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :time, :address)
+  end
 end
